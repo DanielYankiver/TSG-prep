f5747fed3251d4b6916d8e5d1f35df309bcb5bb8
Daniel <dyankiver@gmail.com>
2021-07-18 16:34:29 -0400

    Used next/link to make the about link in header got to about page

--- tsg-prep/src/components/header/index.js
+++ tsg-prep/src/components/header/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 function Header() {
     return (
         <header class="text-gray-600 body-font">
@@ -9,7 +11,9 @@ function Header() {
                     <span class="ml-3 text-xl">Tailblocks</span>
                 </a>
                 <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-                    <a class="mr-5 hover:text-gray-900">First Link</a>
+                    <Link href="/about">
+                        <a class="mr-5 hover:text-gray-900">About</a>
+                    </Link>
                     <a class="mr-5 hover:text-gray-900">Second Link</a>
                     <a class="mr-5 hover:text-gray-900">Third Link</a>
                     <a class="mr-5 hover:text-gray-900">Fourth Link</a>
